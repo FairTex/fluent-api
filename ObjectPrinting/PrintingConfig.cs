493d357ce29489d93c05dad32802542934cad50e
FairTex <makarov906@gmail.com>
2017-11-27 21:03:49 +0500

    done

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using NUnit.Framework;
 
 namespace ObjectPrinting
 {
+    public class Config
+    {
+        public HashSet<Type> ExcludedTypes { get; set; } = new HashSet<Type>();
+        public HashSet<string> ExcludedProperties { get; set; } = new HashSet<string>();
+
+        public Dictionary<object, Func<object, string>> SerializeTypes { get; set; } = new Dictionary<object, Func<object, string>>();
+        public Dictionary<string, Func<object, string>> SerializeProperties { get; set; } = new Dictionary<string, Func<object, string>>();
+
+        public CultureInfo CultureInfoForInt { get; set; }
+        public string MemberName { get; set; }
+    }
+
     public class PrintingConfig<TOwner>
     {
+        public Config Config = new Config();
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -13,16 +31,34 @@ namespace ObjectPrinting
 
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
+            // Не знаю как определить имя obj, чтобы воспользоваться словарем SerializeProperties
             if (obj == null)
                 return "null" + Environment.NewLine;
+
+            var objType = obj.GetType();
+
+            if (Config.SerializeTypes.ContainsKey(objType))
+            {
+                return Config.SerializeTypes[objType](obj) + Environment.NewLine;
+            }
+
+            if (objType == typeof(int) && Config.CultureInfoForInt != null)
+            {
+                return ((int)obj).ToString("c", Config.CultureInfoForInt) + Environment.NewLine;
+            }
+
+            if (Config.ExcludedTypes.Contains(objType))
+            {
+                return "" + Environment.NewLine;
+            }
+            
 
             var finalTypes = new[]
             {
                 typeof(int), typeof(double), typeof(float), typeof(string),
                 typeof(DateTime), typeof(TimeSpan)
             };
-            if (finalTypes.Contains(obj.GetType()))
+            if (finalTypes.Contains(objType))
                 return obj + Environment.NewLine;
 
             var identation = new string('\t', nestingLevel + 1);
@@ -36,6 +72,83 @@ namespace ObjectPrinting
                               nestingLevel + 1));
             }
             return sb.ToString();
+        }
+
+        public PrintingConfig<TOwner> ExcludeType<TPropType>()
+        {
+            Config.ExcludedTypes.Add(typeof(TPropType));
+            return this;
+        }
+
+        public PrintingConfig<TOwner> ExcludeProperty<TPropType>(Expression<Func<TOwner, TPropType>> func)
+        {
+            var memberExpression = (MemberExpression) func.Body;
+            var memberName = memberExpression.Member.Name;
+            Config.ExcludedProperties.Add(memberName);
+            return this;
+        }
+
+        // Возвращаем контекст для свойства типа TPropType
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this, Config);
+        }
+
+        
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> func)
+        {
+            var memberExpression = (MemberExpression) func.Body;
+            Config.MemberName = memberExpression.Member.Name;
+
+            return new PropertyPrintingConfig<TOwner, TPropType>(this, Config);
+        }
+
+    }   
+
+    public class PropertyPrintingConfig<TOwner, TPropType>
+    {
+        public PrintingConfig<TOwner> PrintingConfig;
+        public Config Config;
+        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig, Config config)
+        {
+            PrintingConfig = printingConfig;
+            Config = config;
+        }
+
+        // Указываем конкретный способ сериализации для свойства типа TPropType
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> func)
+        {
+            if (Config.MemberName != null)
+            {
+                if (!Config.SerializeProperties.ContainsKey(Config.MemberName))
+                    Config.SerializeProperties.Add(Config.MemberName, x => func((TPropType) x));
+            }
+            else
+            {
+                if (!Config.SerializeTypes.ContainsKey(typeof(TPropType)))
+                    Config.SerializeTypes.Add(typeof(TPropType), x => func((TPropType)x));
+            }
+
+            return PrintingConfig;
+        }
+    }
+
+    public static class PropertyPrintingConfigExtension
+    {
+        // Метод расширения для типа int
+        public static PrintingConfig<TOwner> UseCulture<TOwner>(this PropertyPrintingConfig<TOwner, int> p,
+            CultureInfo cultureInfo)
+        {
+            p.Config.CultureInfoForInt = cultureInfo;
+            return p.PrintingConfig;
+        }
+
+        // Метод расширения для типа string
+        public static PrintingConfig<TOwner> Cut<TOwner>(this PropertyPrintingConfig<TOwner, string> p, 
+            int count)
+        {
+            p.Config.SerializeTypes.Add(typeof(string), x => ((string)x).Substring(0, Math.Min(count, ((string)x).Length)));
+            return p.PrintingConfig;
         }
     }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,27 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
-	[TestFixture]
-	public class ObjectPrinterAcceptanceTests
-	{
-		[Test]
-		public void Demo()
-		{
-			var person = new Person { Name = "Alex", Age = 19 };
+    [TestFixture]
+    public class ObjectPrinterAcceptanceTests
+    {
+        [Test]
+        public void Demo()
+        {
+            var person = new Person {Name = "Alex", Age = 19};
 
-			var printer = ObjectPrinter.For<Person>();
-				//1. Исключить из сериализации свойства определенного типа
-				//2. Указать альтернативный способ сериализации для определенного типа
-				//3. Для числовых типов указать культуру
-				//4. Настроить сериализацию конкретного свойства
-				//5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-				//6. Исключить из сериализации конкретного свойства
-            
+            var printer = ObjectPrinter.For<Person>()
+                //1. Исключить из сериализации свойства определенного типа
+                .ExcludeType<Guid>()
+                //2. Указать альтернативный способ сериализации для определенного типа
+                .Printing<Guid>()
+                .Using(prop => "1")
+                //3. Для числовых типов указать культуру
+                .Printing<int>()
+                .UseCulture(CultureInfo.CurrentCulture)
+                //4. Настроить сериализацию конкретного свойства
+                .Printing(p => p.Name)
+                .Using(id => id.ToString()) 
+                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Printing<string>()
+                .Cut(100)
+                //6. Исключить из сериализации конкретного свойства
+                .ExcludeProperty(p => p.Id);
+
             string s1 = printer.PrintToString(person);
 
-			//7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию		
-			//8. ...с конфигурированием
-		}
-	}
+            var a = 1;
+
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию		
+            //8. ...с конфигурированием
+        }
+    }
 }
